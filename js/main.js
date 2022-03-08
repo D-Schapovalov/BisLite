d1380c9578410ef7f463bf97a4804d44a4626987
Dmitriy <shapovalov-84@internet.ru>
2022-03-08 23:08:03 +0700

    refrash: works-slider

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ $(function () {
 		autoplaySpeed: 2000,
 		dots: true,
 	});
-	$('.works__slider-inner').slick({
-		slidesToShow: 4,
-		slidesToSkroll: 1,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		prevArrow: '<button class="slick-prev slick-arrow"><img src="icons/arrow_left.svg" alt="arrow_left"></button>',
-		nextArrow: '<button class="slick-next slick-arrow"><img src="icons/arrow_right.svg" alt="arrow_left"></button>',
 
+	$('.works__inner').slick({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<button class="slick-prev slick-arrow"><img src="icons/arrow_left.svg" alt=""></button>',
+		nextArrow: '<button class="slick-next slick-arrow"><img src="icons/arrow_right.svg" alt=""></button>',
 	});
 
 });
